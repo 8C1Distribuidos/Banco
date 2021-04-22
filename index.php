d2aabc43f49b087cd2860898508d2e25aea4203a
JESUS ADRIAN LLANOS JIMENEZ <a17300148@live.ceti.mx>
2021-04-22 13:16:40 -0500

    firtr

---
+++ index.php
@@ -0,0 +1,24 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link rel="stylesheet" href="css/login.css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="fontawesome-free-5.15.3-web/css/all.css" rel="stylesheet">
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+    <h1>BANCO</h1>    <!-- Icon -->
+    <div class="fadeIn first">
+    <i style="margin: 10px;" class="fas fa-piggy-bank fa-7x"></i>
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="card" class="fadeIn second" name="card" maxlength="16" placeholder="No. de tarjeta" required>
+      <input type="text" id="pin" class="fadeIn third" name="pin" placeholder="****" required>
+      <input type="submit" class="fadeIn fourth" value="Iniciar">
+    </form>
+
+  </div>
+</div>
